e9f1f9983acd92660ca99d03a2aff8efcabcec0c
zqxl <416477054@qq.com>
2018-12-24 23:49:07 +0800

    Signed-off-by: zqxl <416477054@qq.com>

---
+++ Leetcode/_100 Same Tree/_100 Same Tree.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* Definition for a binary tree node.*/
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+
+}
+
+int main(){
+	return 0;
+}
