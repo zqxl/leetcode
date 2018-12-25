7319c7f9ff3a4ae603090c5deb7d953d5d81f2b6
zqxl <416477054@qq.com>
2018-12-25 09:21:17 +0800

    Doing 100. Signed-off-by: zqxl <416477054@qq.com>

--- Leetcode/_100 Same Tree/_100 Same Tree.cpp
+++ Leetcode/_100 Same Tree/_100 Same Tree.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<malloc.h>
 /**
 * Definition for a binary tree node.*/
 struct TreeNode {
@@ -7,10 +8,31 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+// 迭代方式，利用栈结构对两个树进行遍历
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+	TreeNode *stack_p = (TreeNode*)malloc(sizeof(TreeNode) * 4096);
+	int size_p = 0;
+	TreeNode *stack_q = (TreeNode*)malloc(sizeof(TreeNode) * 4096);
+	int size_q = 0;
+	int i = 0;
+	while (1){
+		stack_p[i] = *p;
+	}
+	return 0;
+}
+
+// 递归 原文：https ://blog.csdn.net/xishirujin_/article/details/51235733 
+bool isSameTree1(struct TreeNode* p, struct TreeNode* q) {
+	if (p == NULL&&q == NULL)
+		return true;
+	if (p == NULL || q == NULL)
+		return false;
+	return p->val == q->val&&isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
 
 }
 
 int main(){
+	TreeNode *a = (TreeNode*)malloc(sizeof(TreeNode) * 3);
+
 	return 0;
 }
