7958ec9036dbf518f4ca6eded8fc58818f994154
zqxl <416477054@qq.com>
2019-01-08 08:57:02 +0800

    100. Same tree. Finish iterative method.
    Signed-off-by: zqxl <416477054@qq.com>

--- Leetcode/_100 Same Tree/_100 Same Tree.cpp
+++ Leetcode/_100 Same Tree/_100 Same Tree.cpp
@@ -6,19 +6,25 @@ struct TreeNode {
     int val;
     struct TreeNode *left;
     struct TreeNode *right;
+
+	TreeNode(int x) {
+		val = x;
+		left = NULL;
+		right = NULL;
+	}
 };
 
 // 迭代方式，利用栈结构对两个树进行遍历
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-	struct TreeNode *stack_p = (struct TreeNode*)malloc(sizeof(struct TreeNode) * 4096);
+	struct TreeNode *stack_p[4096] ;
 	int size_p = 0;
-	struct TreeNode *stack_q = (struct TreeNode*)malloc(sizeof(struct TreeNode) * 4096);
+	struct TreeNode *stack_q[4096];
 	int size_q = 0;
 	int i = 0;
 	if (p && q){
-		stack_p[size_p] = *p;
+		stack_p[size_p] = p;
 		size_p++;
-		stack_q[size_p] = *q;
+		stack_q[size_q] = q;
 		size_q++;
 	}
 	else if (p == NULL  & q == NULL)
@@ -30,8 +36,8 @@ bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
 		// 判断当前出栈节点数据是否相同
 		size_p--;
 		size_q--;
-		p = &stack_p[size_p];
-		q = &stack_q[size_q];
+		p = stack_p[size_p];
+		q = stack_q[size_q];
 		if (p->val != q->val){
 			return false;
 		}
@@ -39,9 +45,9 @@ bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
 		//判断非空，然后入栈
 		// 右孩子先入栈
 		if (p->right && q->right){
-			stack_p[size_p] = *p->right;
+			stack_p[size_p] = p->right;
 			size_p++;
-			stack_q[size_p] = *q->right;
+			stack_q[size_q] = q->right;
 			size_q++;
 		}
 		else if (p->right == NULL && q->right == NULL);
@@ -49,9 +55,9 @@ bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
 
 		// 左孩子入栈
 		if (p->left && q->left){
-			stack_p[size_p] = *p->left;
+			stack_p[size_p] = p->left;
 			size_p++;
-			stack_q[size_p] = *q->left;
+			stack_q[size_q] = q->left;
 			size_q++;
 		}
 		else if (p->left == NULL && q->left == NULL);
@@ -71,7 +77,13 @@ bool isSameTree1(struct TreeNode* p, struct TreeNode* q) {
 }
 
 int main(){
-	TreeNode *a = (TreeNode*)malloc(sizeof(TreeNode) * 3);
+	TreeNode t1(1), t2(2), t3(3);
+	t1.left = &t2;
+	t1.right = &t3;
+	TreeNode y1(1), y2(2), y3(3);
+	y1.left = &y2;
+	y1.right = &y3;
 
+	isSameTree(&t1, &y1);
 	return 0;
 }
