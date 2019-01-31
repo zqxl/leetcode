c78c80461fc7f6d3e48d76a0dbb337b106d755a6
zqxl <416477054@qq.com>
2019-01-31 10:45:07 +0800

    Finish 204.
    Signed-off-by: zqxl <416477054@qq.com>

--- Leetcode/_204 Count Primes/_204 Count Primes.cpp
+++ Leetcode/_204 Count Primes/_204 Count Primes.cpp
@@ -3,21 +3,53 @@
 #include<string>
 #include<math.h>
 
-bool isPrime(int x) {
-	for (int i = 2; i < x; i++) {
-		if (x%i == 0)
+bool isPrime(int x, int* ra, int rs) {
+	for (int i = 0; ra[i] * ra[i] <= x; i++) {
+		if (x%ra[i] == 0)
 			return false;
 	}
 	return true;
 }
-int countPrimes(int n) {
+int countPrimes1(int n) {
 	if (n < 3) return 0;
+
+	int *ra = (int*)malloc(sizeof(int)*n);
 	int rs = 1;
-	for (int i = 3; i < n; i++) {
-		if (isPrime(i))
-			rs++;
+	ra[0] = 2;
+	for (int i = 3; i < n; i=i+2) {
+		if (isPrime(i, ra, rs))
+			ra[rs++] = i;
 	}
 	return rs;
+}
+/*
+欧拉法
+https://blog.csdn.net/u012102306/article/details/71407105
+*/
+// 执行用时为 8 ms 的范例
+int countPrimes(int n) {
+	if (n <= 2) return 0;
+	int i, count = 1, j;
+	bool *nums = (bool*)malloc(sizeof(bool)*n);
+	//默认都置为质数
+	for (int i = 3; i<n; i += 2) {
+		nums[i] = false;
+	}
+	// 对于每个质数小于根号n的每个质数，将其所有的整数倍置为合数
+	for (i = 3; i*i <= n; i += 2)
+		if (!nums[i])
+			for (j = i*i; j<n; j += i)
+				nums[j] = true;
+	// 统计质数个数
+	for (i = 3; i<n; i += 2)
+		if (!nums[i]) count++;
+
+	return count;
+}
+
+int main() {
+	countPrimes(10);
+	return 0;
 }
 
 
