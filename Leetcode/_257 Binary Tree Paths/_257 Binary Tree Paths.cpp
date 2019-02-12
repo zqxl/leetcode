886f0002f9947279fd3ab028f2498a6f14e70ad3
zqxl <416477054@qq.com>
2019-02-12 09:18:09 +0800

    Doing 257. Signed-off-by: zqxl <416477054@qq.com>

--- Leetcode/_257 Binary Tree Paths/_257 Binary Tree Paths.cpp
+++ Leetcode/_257 Binary Tree Paths/_257 Binary Tree Paths.cpp
@@ -4,4 +4,16 @@
 #include<math.h>
 
 
+char** binaryTreePaths(struct TreeNode* root, int* returnSize) {
+	*returnSize = 3;
+	char **s = (char**)calloc(8, sizeof(char*));
 
+	return s;
+}
+
+int main() {
+	char b[] = "123";
+	char *a = (char*)calloc(4096, sizeof(char));
+	strcat_s(a, 4096, "456");
+	return 0;
+}
