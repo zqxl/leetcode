28e4d71f66d5c801226ed20b2b2c8f6e59378000
zqxl <416477054@qq.com>
2019-05-11 17:47:28 +0800

    正在编写二叉树打印程序。

--- Leetcode/__Tree codes/__Tree codes.cpp
+++ Leetcode/__Tree codes/__Tree codes.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
+#include<malloc.h>
 using namespace std;
 
 // Definition for a binary tree node.
@@ -24,20 +26,69 @@ struct TreeNode *createTreeFromArray(int *nums, int numsSize) {
 	return &nodes[0];
 }
 
+void calTreeDepth(TreeNode *t);
+void setPrintBuffer(TreeNode *t);
+int depth = 0, column = 0;
+int depthMax = 0;
+char **printBuffer = NULL;
 void bt_print(TreeNode *bt) {
+	// 根据树的大小申请内存
+	calTreeDepth(bt);
+	int width = (1 << (depthMax - 1)) * 5;
+	int height = 1 + (depthMax - 1) * 3;
+	printBuffer = (char**)malloc(height *sizeof(char*));
+	for (int i = 0; i < depthMax; i++) {
+		printBuffer[i] = (char*)malloc(width * sizeof(char));
+		memset(printBuffer[i],' ',width);
+		printBuffer[i][width - 1] = '/0';
+	}
+
+
+	setPrintBuffer(bt);
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			printf("%c", printBuffer[i][j]);
+		}
+	}
 
 }
 
-int depth=0;
-void dfs(TreeNode *t) {
-	
+void calTreeDepth(TreeNode *t) {
+	if (t) {
+		depth++;
+		depthMax = depthMax < depth ? depth : depthMax;
+		calTreeDepth(t->left);
+		calTreeDepth(t->right);
+		depth--;
+	}
+}
+
+void setPrintBuffer(TreeNode *t) {
+	if (t) {
+		depth++;
+
+		printBuffer[(depth - 1) * 3][column * 5 - 3] = t->val + 0x30;
+
+		calTreeDepth(t->left);
+		column++;
+		calTreeDepth(t->right);
+		column--;
+		depth--;
+	}
 }
 
 
 int main() {
-	int nums[] = { 5,4,8,11,0,13,4,7,2,0,0,0,0,5,1 };
-	struct TreeNode* root = createTreeFromArray(nums, 15);
-	printf("          / \n         /");
+	int nums[] = { 5,4,8,11,0,13,4}; //,7,2,0,0,0,0,5,1 
+	struct TreeNode* root = createTreeFromArray(nums, sizeof(nums)/sizeof(nums[0]));
+	//dfs(root);
+	bt_print(root);
+
+	printf(" /\n");
+	printf("/\n");
+	for (int i = 0; i < 144; i++) {
+		printf("5");
+	}
 	getchar();
 	return 0;
 }
