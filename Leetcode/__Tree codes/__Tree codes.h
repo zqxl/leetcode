25d9b925703ff9a3fab1ea1e85fb4eeb8683179f
zqxl <416477054@qq.com>
2019-05-12 11:12:28 +0800

    更新readme。

--- Leetcode/__Tree codes/__Tree codes.h
+++ Leetcode/__Tree codes/__Tree codes.h
@@ -16,7 +16,7 @@ struct TreeNode {
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-// 从nums数组生成二叉树。数组元素须为小于等与999的正整数，空节点以numMeansNull表示。
+// 从nums数组生成二叉树。数组元素须为小于等与999的正整数，空节点以宏定义numMeansNull表示。
 struct TreeNode *createTreeFromArray(int *nums, int numsSize);
 // 打印二叉树bt
 void bt_print(TreeNode *bt);
--- Leetcode/__Tree codes/treeCodeTest.cpp
+++ Leetcode/__Tree codes/treeCodeTest.cpp
@@ -1,7 +1,7 @@
 #include"__Tree codes.h"
 
 int main() {
-	int nums[] = { 15,54,87,-1,-1,2,-1,-1,452,10,20,13,2,150,5, -1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
+	int nums[] = { 15,54,87,1,45,20,4,88,452,-1,-1,13,-1,150,5, 6,-1,3,4,5,6,7,8,9,-1,11,12,13,14,15,16 };
 	struct TreeNode* root = createTreeFromArray(nums, sizeof(nums) / sizeof(nums[0]));
 	bt_print(root);
 	getchar();
